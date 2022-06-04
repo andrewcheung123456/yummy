ba991a068b2765041c71d8d87151547613d8393f
zhen <877295761@qq.com>
2022-06-04 14:35:48 +0800

    方法上加@Transactional才会事务托管

--- src/main/java/com/yummy/model/User.java
+++ src/main/java/com/yummy/model/User.java
@@ -2,6 +2,7 @@ package com.yummy.model;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import lombok.Data;
@@ -13,7 +14,7 @@ import lombok.EqualsAndHashCode;
  * </p>
  *
  * @author andrew
- * @since 2022-06-03
+ * @since 2022-06-04
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -23,15 +24,15 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 主键ID
+     * 自增ID
      */
       @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
+    private Integer id;
 
     /**
-     * 姓名
+     * 用户名称
      */
-    private String name;
+    private String userName;
 
     /**
      * 年龄
@@ -42,6 +43,16 @@ public class User implements Serializable {
      * 邮箱
      */
     private String email;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
 
 
 }
